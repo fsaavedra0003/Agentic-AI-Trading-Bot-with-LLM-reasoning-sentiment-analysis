680fe8afa80789d92a12ecd910fc748a658ca220
Macbook <fsaavedra003@gmail.com>
2025-09-02 23:47:17 +0200

    Added new file

--- models/xgboost_model.py
+++ models/xgboost_model.py
@@ -4,3 +4,4 @@
 XGBoost model for Buy/Sell/Hold signals
 
 - Trains a multi-class classifier on engineered features.
+- If 'label' is missing in the input CSV, derives a weak label from avg_sentiment.
